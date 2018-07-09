725acd119b1b2928bd451b399a9b13db3abfcbdd
Vadim Graboys <dimva13@gmail.com>
2018-07-08 20:50:07 -0700

    wip

--- building_stats.py
+++ building_stats.py
@@ -9,7 +9,7 @@ from fileutil import load, dump
 
 
 def main():
-    features = load('data/buildings.geojson')
+    features = load('generated/building_zoning.geojson')
 
     r = Results()
     r.units = 0
@@ -23,9 +23,18 @@ def main():
 
     single_family = []
     all_homes = []
+    homes_0_1 = []
+    homes_2plus = []
+    homes_3plus = []
+    no_homes = []
 
-    for obj in features:
+    for i, obj in enumerate(features):
+        print i, '/', len(features)
+
         prop = obj['properties']
+        if prop.get('zoning') is None:
+            continue
+
         units = int(prop['resunits'])
         year = int(prop['yrbuilt'])
         r.max_year = max(year, r.max_year)
@@ -44,12 +53,21 @@ def main():
         elif units >= 20:
             r.units_20 += units
 
+
         s = shape(obj['geometry'])
         area = sq_ft(s)
 
         prop['fill'] = color(units)
         if prop['landuse'] == 'OpenSpace':
             prop['fill'] = color(-1)
+        elif units in (0, 1):
+            if units == 0:
+                no_homes.append(obj)
+            homes_0_1.append(obj)
+        else:
+            if units > 2:
+                homes_3plus.append(obj)
+            homes_2plus.append(obj)
         all_homes.append(obj)
 
     print 'year built histogram', '-'*60
@@ -61,6 +79,10 @@ def main():
 
     dump('generated/single_family_homes.geojson', single_family)
     dump('generated/all_homes.geojson', all_homes)
+    dump('generated/no_homes.geojson', no_homes)
+    dump('generated/homes_0_1.geojson', homes_0_1)
+    dump('generated/homes_2plus.geojson', homes_2plus)
+    dump('generated/homes_3plus.geojson', homes_3plus)
 
 if __name__ == '__main__':
     main()
