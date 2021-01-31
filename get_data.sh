062395285d27dd2dd9595a9a04d9097c6b690eaa
Vadim Graboys <dimva13@gmail.com>
2021-01-30 21:12:47 -0700

    update urls

--- get_data.sh
+++ get_data.sh
@@ -2,8 +2,8 @@
 
 mkdir -p data
 
-curl 'https://data.sfgov.org/api/geospatial/xvjh-uu28?method=export&format=GeoJSON' > data/zoning.geojson
-curl 'https://data.sfgov.org/api/geospatial/iddb-5nzh?method=export&format=GeoJSON' > data/height.geojson
+curl 'https://data.sfgov.org/api/geospatial/3i4a-hu95?method=export&format=GeoJSON' > data/zoning.geojson
+curl 'https://data.sfgov.org/api/geospatial/h6dm-b62f?method=export&format=GeoJSON' > data/height.geojson
 curl 'https://data.sfgov.org/api/geospatial/us3s-fp9q?method=export&format=GeoJSON' > data/lots.geojson
 curl 'https://data.sfgov.org/api/geospatial/ynuv-fyni?method=export&format=GeoJSON' > data/buildings.geojson
 curl 'https://data.sfgov.org/api/views/wv5m-vpq2/rows.csv?accessType=DOWNLOAD' > data/home_values.csv
